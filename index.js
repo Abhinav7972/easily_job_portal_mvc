f10964b402e79cfc2733eaad47fd04ee84ea9879
abhinav soni <abhinavsoni316@gmail.com>
2026-07-01 17:18:40 +0530

    configured ejs and ejs layout in index

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from 'express'
 import path  from 'path'
 import HomeController from './src/controllers/home.controller.js';
+import EjsLayouts from 'express-ejs-layouts';
+
 const server = new express();
 const home = new HomeController();
-server.use(express.static('src/views'))
-server.set(path.join(path.resolve(),'src','views'))
 
-server.get('/',(req,res)=> home.getHome)
+//server.use(express.static('src/views'));
+server.set('views',path.join(path.resolve(),'src','views'));
+
+server.set('view engine','ejs')
+server.use(EjsLayouts)
+
+server.set('layout','layout/layout');
+
+server.get('/',home.Homepage)
 
 
 server.listen(3000,()=>console.log('server began at 3000'))
