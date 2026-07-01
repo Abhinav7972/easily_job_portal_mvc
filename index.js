fa8f40737cbaeaa680b78f2f49f206cddb255ee7
abhinav soni <abhinavsoni316@gmail.com>
2026-07-01 14:45:02 +0530

    set up path,express.static and setup home controller in index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
-
+import path  from 'path'
+import HomeController from './src/controllers/home.controller.js';
 const server = new express();
+const home = new HomeController();
+server.use(express.static('src/views'))
+server.set(path.join(path.resolve(),'src','views'))
 
-
-
-server.get('/',(req,res)=> res.send('welcome to job portal good luck'))
+server.get('/',(req,res)=> home.getHome)
 
 
 server.listen(3000,()=>console.log('server began at 3000'))
