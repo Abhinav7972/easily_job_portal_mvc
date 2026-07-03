a8e299a91881d7358693ff4e6f04c5db10e913f3
abhinav soni <abhinavsoni316@gmail.com>
2026-07-03 12:11:39 +0530

    setup route file to index

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import express from 'express'
 import path  from 'path'
-import HomeController from './src/controllers/home.controller.js';
 import EjsLayouts from 'express-ejs-layouts';
+import homeRoute from './src/routes/homeRoute.js';
+
 
 const server = new express();
-const home = new HomeController();
+
 
 //server.use(express.static('src/views'));
+
+//set up views path 
 server.set('views',path.join(path.resolve(),'src','views'));
 
+//setup view engine and ejs layput middleware
 server.set('view engine','ejs')
 server.use(EjsLayouts)
 
+//setup path  to layout.ejs
 server.set('layout','layout/layout');
 
-server.get('/',home.Homepage)
+//route
+server.use('/',homeRoute) //home page 
 
 
 server.listen(3000,()=>console.log('server began at 3000'))
